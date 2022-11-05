ea2108fd3ef2ced767fba5f1946303ad876824ab
nemkicsiaron <aronfszabo@gmail.com>
2022-11-05 18:56:40 +0100

    funi jif

--- src/Surprise.js
+++ src/Surprise.js
@@ -5,28 +5,14 @@ const Surprise = () => {
     let speech = new SpeechSynthesisUtterance();
     const voices = window.speechSynthesis.getVoices();
 
-    speech.lang = 'en-GB';
+    speech.lang = 'ja-JN';
     speech.text = secret;
-    speech.pitch = 2;
-    speech.rate = 4;
+    speech.pitch = Math.random() * 1.7;
+    speech.rate = Math.random() * 8.5;
     speech.volume = 1;
-    console.log(voices)
-    let up = false;
-
-    const pitch = () => {
-        window.speechSynthesis.pause();
-        speech.pitch += up ? 0.05 : -0.05;
-        up = speech.pitch <= 0;
-        speech.rate = Math.floor(Math.random() * (9.5 - 0 + 1) )
-        window.speechSynthesis.resume();
-        setTimeout(pitch(), 200);
-    };
-
-    pitch();
-
+    window.speechSynthesis.speak(speech);
     return(<div className="surprise">
-        <h1>LAJOS</h1>
-        <script> window.speechSynthesis.speak(speech);</script>
+        <script></script>
     </div>)
 };
 
