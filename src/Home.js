09fc445395a05c70b155c9bb3fa804de2d826927
nemkicsiaron <aronfszabo@gmail.com>
2022-11-05 19:59:56 +0100

    home page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import "./Home.css"
 
 const Home = () => {
-    return (<div>
-        <h1>🎉</h1>
-        <Link to={"/surprise"}>Try this</Link>
+    return (<div className="homeboi">
+        <img className="boom" src="https://media0.giphy.com/media/3rYxjPwF5i9mALN1UM/giphy.gif?cid=ecf05e47rndwj9vhq1ybgcmljgcamhhbtsv4q1mbx14v4sl1&rid=giphy.gif&ct=s" alt="party fireworks" />
+        <Link to={"/surprise"}>Don't click this</Link>
     </div>);
 };
 
