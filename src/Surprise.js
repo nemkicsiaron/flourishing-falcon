dc7dfc527e9dc865da63062ba4b2d3b8b80f2870
nemkicsiaron <aronfszabo@gmail.com>
2022-11-05 19:15:20 +0100

    fixed shit

--- src/Surprise.js
+++ src/Surprise.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import secret from "./secret";
 import "./Surprise.css"
 
 const Surprise = () => {
     let speech = new SpeechSynthesisUtterance();
     const voices = window.speechSynthesis.getVoices();
+    const [glassed, setGlassed] = useState(true);
 
     speech.lang = 'ja-JN';
     speech.text = secret;
     speech.pitch = Math.random() * 1.7;
     speech.rate = Math.random() * 8.5;
     speech.volume = 1;
-    window.speechSynthesis.speak(speech);
+
+
+    const pop = () => {
+        setGlassed(false)
+        window.speechSynthesis.speak(speech);
+    }
+
     return(<div className="surprise">
-        <script></script>
+        {glassed && <div className="glass">
+            <button className="button" onClick={pop}>🎁</button>
+        </div>}
     </div>)
 };
 
